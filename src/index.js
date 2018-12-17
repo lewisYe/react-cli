ddd5958aa95d4128403eb0453d3b7dbdcade4019
lewisYe <supermasterye@163.com>
2018-12-17 14:32:58 +0800

    添加错误捕获

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App';
 import './index.css';
+import ErrorBoundary from './components/errorBoundary';
 
 ReactDOM.render(
-  <App/>,
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
   document.getElementById('app')
 );
