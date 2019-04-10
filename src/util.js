dd0fad3c320b7f1093e68812000624b61b224ab9
lewisYe <supermasterye@163.com>
2019-04-10 11:05:12 +0800

    bugFixed:RouteWithSubRoutes

--- src/util.js
+++ src/util.js
@@ -5,6 +5,7 @@ export const RouteWithSubRoutes = (route) => {
   return (
     <Route
       path={route.path}
+      exact={route.exact || false}
       render={props => (
         <route.component {...props} routes={route.routes} />
       )}
